6e23748b7c0a8f1e1515e67e300af6358fde3de7
Nika Maisuradze <103008084+Flexyy19@users.noreply.github.com>
2025-03-18 22:19:40 +0400

    add exercises

--- exercises.php
+++ exercises.php
@@ -20,4 +20,33 @@ for ($i = 1; $i <= $rows; $i++) {
     }
     echo "<br>";
 }
+
+$color = array('white', 'green', 'red', 'blue', 'black');
+
+echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the " 
+    . $color[2] . " carpet, the " . $color[1] . " lawn, the " . $color[0] . " house, the leaden sky. "
+    . "The new president and his first lady. - Richard M. Nixon";
+
+    $color = array('white', 'green', 'red');
+
+echo implode(", ", $color) . ",<br><br>";
+
+foreach ($color as $c) {
+    echo "$c<br>";
+}
+
+
+$x = array(1, 2, 3, 4, 5);
+
+echo "Before deletion: ";
+print_r($x);
+
+unset($x[3]);
+
+$x = array_values($x);
+
+echo "<br>After deletion: ";
+print_r($x);
+
+
 ?>
